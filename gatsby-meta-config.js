7de6080194708257a8c429fc6a9988eaafe57e81
szto <sztolabs@gmail.com>
2020-07-08 12:49:58 +0900

    feat: introduction 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: `szto Blog`,
   description: `프로그래머 with python・django・devops・serverless・aws`,
   author: `szto`,
-  introduction: `기술이 이롭게 하는 세상`,
+  introduction: `기술이 이롭게 하는 세상, 개발 쉽게하고... 잘하자...`,
   siteUrl: `https://sztolabs.netlify.com`, // Your blog site url
   social: {
     twitter: `sztolabs`, // Your Twitter account
